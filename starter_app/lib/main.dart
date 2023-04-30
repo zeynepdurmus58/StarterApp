4bc305b57d3eed0b75e9d6ef07f79188c2ff5df8
zeynepdurmus <zeynep.durmus@spike.com.tr>
2023-04-30 21:41:34 +0300

    added router for home screen

--- starter_app/lib/main.dart
+++ starter_app/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:starter_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// GoRouter configuration
+final _router = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => HomeScreen(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
